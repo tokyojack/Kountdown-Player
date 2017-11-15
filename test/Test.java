ee860096dc4f5fd2a133300fdd85112af0256776
Jack Clarke <tokyojack2001@yahoo.com>
2017-11-15 01:38:56 +0000

    Update test.java & README.md

--- test/Test.java
+++ test/Test.java
@@ -4,21 +4,32 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Test extends JavaPlugin implements Listener {
 
-	KountdownPlayer kountdownPlayer = new KountdownPlayer(this) {
+	KountdownPlayer logoutPlayerKountdown = new KountdownPlayer(this) {
+
+		@Override
+		public void start(Player player) {
+			player.sendMessage("You've started your logout");
+		}
 
 		@Override
 		public void tick(Player player, int countdown) {
-			player.sendMessage("Time left: " + countdown);
+			player.sendMessage("Logging out in " + countdown);
 
 		}
 
 		@Override
-		public void finished(Player player) {
+		public void stop(Player player) {
+			player.sendMessage("You've stopped your logout");
+		}
+
+		@Override
+		public void finish(Player player) {
 			player.sendMessage("Done!");
 		}
 
@@ -31,9 +42,21 @@ public class Test extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void playerJoin(PlayerJoinEvent event) {
-		// if some condition...
-		this.kountdownPlayer.addPlayer(event.getPlayer(), 10);
+		// Note: the void would be called in the command class, this is just for
+		// testing
+		logoutCommand(event.getPlayer());
+	}
+
+	public void logoutCommand(Player player) {
+		logoutPlayerKountdown.startPlayer(player, 10 * 20); // 10 seconds
+	}
+
+	@EventHandler
+	public void playerMove(PlayerMoveEvent event) {
+		Player player = event.getPlayer();
+
+		if (logoutPlayerKountdown.containsPlayer(player))
+			logoutPlayerKountdown.stopPlayer(player);
 	}
 
 }
-
